d4d7aff452c8ba34610fdf9cffe21da4f5619ead
Jonah Kollner <jkollner@2-H-5.42heilbronn.de>
2023-09-18 11:07:11 +0200

    started ex01 and inserted new function names

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 10:04:46 by jkollner          #+#    #+#             */
+/*   Updated: 2023/09/18 11:05:40 by jkollner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(){
+	std::cout << "Default constructor called" << std::endl;
+	this->fpn_value = 0;
+}
+
+Fixed& Fixed::operator= ( const Fixed &fix ){
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fpn_value = fix.getRawBits();
+	return (*this);
+}
+
+Fixed::Fixed( Fixed &fix){
+	std::cout << "Copy constructor called" << std::endl;
+	// *this = fix;
+	this->fpn_value = fix.getRawBits();
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits ( void ) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fpn_value);
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &fix) {
+
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkollner <jkollner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 09:58:26 by jkollner          #+#    #+#             */
+/*   Updated: 2023/09/18 11:06:21 by jkollner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#pragma once
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+	private:
+		int fpn_value;
+		static const int fractional_bits = 8;
+	public:
+		Fixed(); // default constructor
+		Fixed( const int val ); // constructor overload with parameter
+		Fixed( const float va ); // constructor oeload wiht parameter
+		~Fixed(); // desturctor
+		Fixed( Fixed &fix); // copy constructor
+		Fixed& operator= (const Fixed &fix); // copy assingment overload
+		int getRawBits( void ) const; // member function
+		void setRawBits ( int const raw ); // member function
+		float toFloat( void ) const;
+		int toInt( void ) const;
+};
